54ca7b808160953fb30597f1bdba3b3780454c85
Ni1s-B <nils.bergholtz@learnet.se>
2022-09-02 11:01:33 +0200

    jag heter

---
+++ test.js
@@ -0,0 +1,11 @@
+//console.log("Hello world")
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
+let name = prompt("enetr your name:");
+console.log(name)*/
+for(let i = 0; i < 3 ; i++)
+{
+    console.log("hello")
+}
+
